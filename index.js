0d85e6ac4d2ed725d277786bc516000915ceb031
Rowen Roel Borgonia <rrborgonia@gmail.com>
2022-04-26 00:22:50 +0800

    seventh commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dotenv = require('dotenv');
 
 //mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect(`${process.env.MONGO_URI}`, {
+mongoose.connect(process.env.MONGO_URI, {
    useNewUrlParser: true,
    useUnifiedTopology: true,
 }).then( () => {
