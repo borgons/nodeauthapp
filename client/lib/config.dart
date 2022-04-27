8b1333f574839c87ed5c6c2babacce662def6e8a
Rowen Roel Borgonia <rrborgonia@gmail.com>
2022-04-27 21:18:27 +0800

    add client

---
+++ client/lib/config.dart
@@ -0,0 +1,9 @@
+class Config {
+  static const String appName = 'Shopping App';
+  static const String apiURL = 'flutternodeauthjs.herokuapp.com'; // PROD_URL or HEROKU
+  static const loginAPI = '/users/login';
+  static const registerAPI = '/users/register';
+  static const userProfileAPI = 'users/user-Profile';
+
+  // if not deploy == '192.168.1.4 for DEVELOPMENT
+}
